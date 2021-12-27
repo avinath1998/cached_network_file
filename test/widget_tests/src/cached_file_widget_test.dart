4aca5dcf0261817a143397de1b00d412a91479ea
Avinath Gunasekara <avinathgunasekara@Avinaths-MacBook-Pro.local>
2021-12-27 12:07:56 +0530

    removed unwanted code

--- test/widget_tests/src/cached_file_widget_test.dart
+++ test/widget_tests/src/cached_file_widget_test.dart
@@ -1,19 +1,6 @@
-import 'dart:io';
-
-import 'package:cached_network_file/cached_network_file.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-
-import '../../unit_tests/src/cached_file_bloc_test.dart';
 
 void main() {
-  final mockCacheManager = MockCacheManager();
-  final mockFile = MockFile();
-  final mockFileInfo = MockFileInfo();
-
   group("CachedFileWidget Tests : ", () {
     group("Normal Tests: ", () {});
   });
